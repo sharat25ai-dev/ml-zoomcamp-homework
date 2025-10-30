fd06ce4e10cef00a5ab39b372746af0a3bc94098
sharat25ai-dev <sharat25.ai@gmail.com>
2025-10-30 22:02:08 +0000

    Homework 5

--- Homework-5/predict.py
+++ Homework-5/predict.py
@@ -1,18 +1,24 @@
 import pickle
-import sklearn
+
+import uvicorn
+from fastapi import FastAPI
+
+from typing import Dict, Any
+
+app = FastAPI(title="Lead Prediction API")
 
 with open('pipeline_v1.bin', 'rb') as f_in:
     pipeline = pickle.load(f_in)
 
-data = {
-    "lead_source": "paid_ads",
-    "number_of_courses_viewed": 2,
-    "annual_income": 79276.0
-}
+@app.post('/predict')
+def predict(data: Dict[str, Any]):
+    lead = float(pipeline.predict_proba(data)[0, 1])
+    return {
+        "lead probability": lead,
+        "lead status": bool(lead >= 0.5)
+    }
 
-lead = pipeline.predict_proba(data)[0, 1]
-print(f'Probability of Successful Lead: {lead}')
-if lead > 0.5:
-    print("This lead is likely to be successful.")
-else:
-    print("This lead is unlikely to be successful.")
+
+if __name__ == '__main__':
+    uvicorn.run(app, host="0.0.0.0", port=9696)
+
